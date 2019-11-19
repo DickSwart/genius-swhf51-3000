37390c155b3a9ef208b4a4763d024ffaffbd71bb
Dick Swart <dick.swart@me.com>
2019-11-19 20:54:56 +1300

    test update

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,16 +3,16 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('genius-swhf51-3000', function () {
-    describe('Routs', () => {
+    describe('routs', () => {
         it('should have an root route', async done => {
             const response = await request.get('/')
             expect(response.status).toBe(200)
             done()
         })
 
-        it('should return 304 for swagger', async done => {
+        it('should return 301 for swagger', async done => {
             const response = await request.get('/api/docs')
-            expect(response.status).toBe(304)
+            expect(response.status).toBe(301)
             done()
         })
 
